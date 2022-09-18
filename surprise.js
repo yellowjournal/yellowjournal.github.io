84ecaaf80124aeca1af3657e82d8564926d2167a
Lil Nell <yellowjournalit@protonmail.com>
2022-09-18 11:30:20 -0400

    Add surprise

---
+++ surprise.js
@@ -0,0 +1,7 @@
+function ready() {
+    if(Math.random() * 10 < 1) {
+        document.getElementById("motto").innerHTML = "\"Pushing Web0 Brutalist Chic Since 1912\"";
+    }
+}
+
+window.addEventListener("DOMContentLoaded", ready);
