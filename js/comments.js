3f69d022deb6d5a8cc82efe13f7d759aaf86df25
Lil Nell <yellowjournalit@protonmail.com>
2023-02-01 12:59:57 -0500

    add more comments

--- js/comments.js
+++ js/comments.js
@@ -34,6 +34,42 @@ let COMMENTS = [
     {
         name: "ConcernedCitizen99",
         body: "I find this repulsive"
+    },
+    {
+        name: "Lawnie",
+        body: "What the fuck is 'beanol'?"
+    },
+    {
+        name: "CoachDonne",
+        body: "I've been coaching B-Ball going on 45 years, and I've never seen game like this!"
+    },
+    {
+        name: "Sissyfuss",
+        body: "Took a break from work (rock pushing factory lol) to read this... good stuff haha"
+    },
+    {
+        name: "EruditeDragon",
+        body: "oh i get it... because the website is yellow"
+    },
+    {
+        name: "SmackinGraphix",
+        body: "This website is pretty good if you're into web0 brutalist chic..."
+    },
+    {
+        name: "John D. Matthews",
+        body: "I didn't do anything wrong"
+    },
+    {
+        name: "John D. Matthews",
+        body: "Literally what"
+    },
+    {
+        name: "GoldbrickinBastard77",
+        body: "Ohh, these cats are cooking! Ah, ooh, ahh, *snaps*"
+    },
+    {
+        name: "Drake",
+        body: "It's drizzy... I got locked out of some accounts and need just a few hundred to sort out my affairs... I'll reward you tenfold if you help me out!"
     }
 ];
 
@@ -51,7 +87,14 @@ function getComments() {
         if(Math.random() < 0.5) {
             numComments += 1;
             if(!!nestComment) {
-                comment.body = `Hi ${nestComment.name}... ` + comment.body;
+                /* commenters will always be mean to John D. Matthews */
+                if(nestComment.name === "John D. Matthews") {
+                    comment.body = "I hate you so much man";
+                } else if(nestComment.body === "I hate you so much man") {
+                    comment.body = "We all hate you John D. Matthews";
+                } else {
+                    comment.body = `Hi ${nestComment.name}... ` + comment.body;
+                }
                 nestComment.replies.push(comment);
             } else {
                 selected.push(comment);
